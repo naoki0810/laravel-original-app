87c5333bcf7120725455b5b2a86faf79b2c088d0
naoki0810 <naoki.komiya.98ss@gamil.com>
2024-03-10 14:53:04 +0900

    kadai

--- config/admin.php
+++ config/admin.php
@@ -84,7 +84,7 @@ return [
     |
     | Html title for all pages.
     |
-    */
+    */ 
     'title' => 'Admin',
 
     /*
@@ -95,7 +95,7 @@ return [
     | If your page is going to be accessed via https, set it to `true`.
     |
     */
-    'https' => env('ADMIN_HTTPS', false),
+    'https' => env('ADMIN_HTTPS', true),
 
     /*
     |--------------------------------------------------------------------------
